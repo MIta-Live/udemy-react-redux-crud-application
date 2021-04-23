a9e1f6069fe0c6b3e0a893541eb606e0d4904f3f
kazu <kazuhiro.mita@livelynx.co.jp>
2021-04-23 11:12:17 +0900

    implemnt event new component

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash'
+import { Link } from 'react-router-dom'
 import { readEvents } from '../actions';
 class EventsIndex extends Component {
   componentDidMount() {
@@ -18,8 +19,8 @@ class EventsIndex extends Component {
   }
 
   render() {
-
       return (
+        <React.Fragment>
         <table>
           <thead>
             <tr>
@@ -33,6 +34,9 @@ class EventsIndex extends Component {
             {this.renderEvents()}
           </tbody>
         </table>
+
+        <Link to="/events/new">New Event</Link>
+        </React.Fragment>
       )
     }
   }
